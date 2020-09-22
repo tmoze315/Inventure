0ea7a10ba33df68f9505edc1f12ef60a608f6f64
tmoze315 <tmoze315@gmail.com>
2020-09-22 23:01:14 +0100

    Initial commit
    Real basics of the bot setup. Need to sort out data storage next.
    
    Install dependencies with:
    npm install
    
    Build and run with:
    npm start
    
    Note that my discord key is commited. I’ll move this into an environment variable at some point too

---
+++ adventure.ts
@@ -0,0 +1,44 @@
+import { Message, MessageEmbed, EmbedFieldData } from "discord.js";
+
+class Adventure {
+    constructor(private message: Message) { }
+
+    // stats([first, last]: [string?, string?]) {
+    //     this.message.channel.send('Starting your adventure!');
+    // }
+
+    stats() {
+        const embed = new MessageEmbed()
+            // Set the title of the field
+            .setTitle(`Character Sheet for: ${this.message.author.username}`)
+            .setFooter(`Active bonus: ( 0  | 0  | 0  | 0  | 2  )\nStats: 0% | EXP: 40% | Credits: 0%`)
+            // Set the color of the embed
+            .setColor('DARK_NAVY')
+            // Set the main content of the embed
+            .setDescription(`A level **104 Berserker**
+
+            Berserkers have the option to **rage** and add big bonuses to attacks, but fumbles hurt. Use the rage command when attacking in an adventure.
+
+            \`\`\`js
+Rebirths: 15
+Max Level: 115
+\`\`\`
+`)
+            .addFields([
+                <EmbedFieldData>{
+                    name: 'Stats',
+                    value: `• Attack: **243** [+53]\n• Charisma: **122** [+0]\n• Intelligence: **124** [+2]`,
+                    inline: true,
+                },
+                <EmbedFieldData>{
+                    name: 'Attributes',
+                    value: `• Dexterity: **109**\n• Luck: **139**`,
+                    inline: true,
+                },
+            ]);
+
+        this.message.channel.send(embed);
+    }
+}
+
+export { Adventure };
---
+++ index.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+import { Message, Client } from 'discord.js';
+import { Adventure } from './adventure';
+
+// Create an instance of a Discord client
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', (message: Message) => {
+    const prefix = '-';
+
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args?.shift()?.toLowerCase();
+
+    if (!command) {
+        return;
+    }
+
+    const adventure = new Adventure(message);
+
+    if (typeof (adventure as any)[command] === 'function') {
+        (adventure as any)[command](args);
+    } else {
+        message.channel.send(`Unable to find command '${command}'`);
+    }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+client.login('NzU4MDQzMTM3Mjg4NTY4ODMz.X2pMrw.IQHhyVHCWX1Ukv2weo1erox1MZE');
